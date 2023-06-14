b8661d8e693228d744dc0516a2ee528a98c05812
SSB4455 <741086@gmail.com>
2023-06-15 07:18:01 +0800

    实现ERC721设置uri属性

--- buildspace_nft-based-game/scripts/run.js
+++ buildspace_nft-based-game/scripts/run.js
@@ -11,6 +11,16 @@ const main = async () => {
 	);
 	await gameContract.deployed();
 	console.log("Contract deployed to:", gameContract.address);
+
+	let txn;
+	// We only have three characters.
+	// an NFT w/ the character at index 1 of our array.
+	txn = await gameContract.mintCharacterNFT(1);
+	await txn.wait();
+
+	// Get the value of the NFT's URI.
+	let returnedTokenUri = await gameContract.tokenURI(0);
+	console.log("Token URI:", returnedTokenUri);
 };
 
 const runMain = async () => {
