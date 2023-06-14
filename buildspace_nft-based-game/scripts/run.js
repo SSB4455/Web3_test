a99a4719caab4deff382c24b6999ff6b9d119eca
SSB4455 <741086@gmail.com>
2023-06-14 16:30:01 +0800

    增加NFT-based-game项目

---
+++ buildspace_nft-based-game/scripts/run.js
@@ -0,0 +1,26 @@
+const main = async () => {
+	const contractFactory = await hre.ethers.getContractFactory('MyNFTsBasedGame');
+	const gameContract = await contractFactory.deploy(
+		["李承远", "Chibimaruko", "Pikachu"],			// Names
+		["https://img-qn.51miz.com/preview/element/00/01/12/94/E-1129426-CB628E6F.jpg!/quality/90/unsharp/true/compress/true/fwfh/800x800",	// Images
+		"https://zh.wikipedia.org/wiki/%E6%AB%BB%E6%A1%83%E5%B0%8F%E4%B8%B8%E5%AD%90#/media/File:%E6%AB%BB%E6%A1%83%E5%B0%8F%E4%B8%B8%E5%AD%90.jpg",
+		"https://i.imgur.com/WMB6g9u.png"],
+		[500, 30, 100],									// HP values
+		[500, 50, 25],									// Attack damage values
+		["master", "warrior", "elf"]					// Attack damage values
+	);
+	await gameContract.deployed();
+	console.log("Contract deployed to:", gameContract.address);
+};
+
+const runMain = async () => {
+	try {
+		await main();
+		process.exit(0);
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
+};
+
+runMain();
